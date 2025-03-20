b00066cdab2b566a29fc1c21ecc988eb2991f2d7
Fabien Amann <fabien@feub.net>
2025-03-20 10:48:10 +0100

    refactor: in ScanType the barcode field has now a number only constraint

--- src/Form/ScanType.php
+++ src/Form/ScanType.php
@@ -4,16 +4,24 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ScanType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('barcode', TextType::class, [])
+            ->add('barcode', TextType::class, [
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^\d*$/',
+                        'message' => 'Barcode can only contain numbers'
+                    ])
+                ]
+            ])
             ->add('search', SubmitType::class, [
                 'label' => 'search'
             ])
