e3aeeed0a7fd3cb7e97704390744c833b87d4837
Fabien Amann <fabien@feub.net>
2025-04-10 15:39:34 +0200

    client/feat: SiteHeader displays a breadcrumb

--- client/src/components/SiteHeader.tsx
+++ client/src/components/SiteHeader.tsx
@@ -1,7 +1,10 @@
+import { useLocation } from "react-router";
 import { Separator } from "./ui/separator";
 import { SidebarTrigger } from "./ui/sidebar";
 
 export function SiteHeader() {
+  const location = useLocation();
+
   return (
     <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -10,7 +13,10 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="text-base font-medium">LibTrack</h1>
+        <h1 className="text-base font-medium">
+          <span className="text-neutral-500">LibTrack:</span>{" "}
+          <span className="font-bold">{location.pathname}</span>
+        </h1>
       </div>
     </header>
   );
