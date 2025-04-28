c814cf02ae28e58ed6dbe132ed3918b76705cd0d
Fabien Amann <fabien@feub.net>
2025-04-28 11:19:24 +0200

    client/feat: rotating loader

--- client/src/components/TheLoader.tsx
+++ client/src/components/TheLoader.tsx
@@ -3,7 +3,7 @@ import { Loader } from "lucide-react";
 export default function TheLoader({ style }: { style: string }) {
   return (
     <div className={`flex gap-x-2 text-neutral-500 ${style}`}>
-      <Loader /> Loading
+      <Loader className="animate-spin-slow" /> Loading
     </div>
   );
 }
