e9f6f7e0145d4f256e66fa441a655e0904ba8641
Fabien Amann <fabien@feub.net>
2025-06-02 16:19:59 +0200

    client/feat: new TopSectionCards in stats with releaeses and artists count

---
+++ client/src/components/stats/TopSectionCards.tsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from "react";
+import { validateApiResponse, handleApiError } from "@/utils/errorHandling";
+import { api } from "@/utils/apiRequest";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import TheLoader from "@/components/TheLoader";
+
+const apiURL = import.meta.env.VITE_API_URL;
+
+export default function TopSectionCards() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [releasesCount, setReleasesCount] = useState<number>(0);
+  const [artistsCount, setArtistsCount] = useState<number>(0);
+
+  const getReleasesCount = async () => {
+    setIsLoading(true);
+    try {
+      const response = await api.get(`${apiURL}/api/stats/releases/count`);
+
+      const data = await validateApiResponse(
+        response,
+        "Fetching releases count",
+      );
+
+      setReleasesCount(data.data.count);
+    } catch (error) {
+      handleApiError(error, "Fetching releases count");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const getArtistsCount = async () => {
+    setIsLoading(true);
+    try {
+      const response = await api.get(`${apiURL}/api/stats/artists/count`);
+
+      const data = await validateApiResponse(
+        response,
+        "Fetching artists count",
+      );
+
+      setArtistsCount(data.data.count);
+    } catch (error) {
+      handleApiError(error, "Fetching artists count");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getReleasesCount();
+    getArtistsCount();
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <TheLoader style="my-4" />
+      ) : (
+        <div className="flex flex-row gap-4 mb-4">
+          <Card className="w-[250px] @container/card">
+            <CardHeader>
+              <CardDescription>Total Releases</CardDescription>
+              <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                {releasesCount}
+              </CardTitle>
+            </CardHeader>
+          </Card>
+          <Card className="w-[250px] @container/card">
+            <CardHeader>
+              <CardDescription>Total Artists</CardDescription>
+              <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                {artistsCount}
+              </CardTitle>
+            </CardHeader>
+          </Card>
+        </div>
+      )}
+    </>
+  );
+}
--- client/src/pages/StatsPage.tsx
+++ client/src/pages/StatsPage.tsx
@@ -1,5 +1,6 @@
 import FormatPieChartCard from "@/components/stats/FormatPieChartCard";
 import GenreBarChartCard from "@/components/stats/GenreBarChartCard";
+import TopSectionCards from "@/components/stats/TopSectionCards";
 
 export default function StatsPage() {
   return (
@@ -7,6 +8,9 @@ export default function StatsPage() {
       <div className="flex items-center justify-between mb-4">
         <h2 className="font-bold text-3xl">Statistics</h2>
       </div>
+
+      <TopSectionCards />
+
       <div className="flex gap-4">
         <FormatPieChartCard />
         <GenreBarChartCard />
