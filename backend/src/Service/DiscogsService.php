041be98e810dda7a00f2be415b359aaada1aefeb
Fabien Amann <fabien@feub.net>
2025-06-15 11:30:44 +0200

    backend/feat: new searchRelease() method for Discogs service

--- backend/src/Service/DiscogsService.php
+++ backend/src/Service/DiscogsService.php
@@ -83,4 +83,46 @@ class DiscogsService
       throw new \Exception('Error fetching release data: ' . $th->getMessage(), 0, $th);
     }
   }
+
+  /**
+   * Undocumented function
+   *
+   * @param string $by -> title,release_title,artist,genre,country,year,format,barcode
+   * @param string $search
+   * @return array
+   */
+  public function searchRelease(string $by, string $search): array
+  {
+    try {
+      $response = $this->httpClient->request('GET', $this->endpoint . 'database/search', [
+        'query' => [
+          $by => $search,
+          'key' => $this->discogs_key,
+          'secret' => $this->discogs_secret
+        ],
+        'headers' => [
+          'User-Agent' => $this->userAgent
+        ]
+      ]);
+
+      if ($response->getStatusCode() === 200) {
+        $response = $response->toArray();
+        $releases = [];
+
+        foreach ($response['results'] as $rel) {
+          $releaseData = $this->getReleaseById($rel['id']);
+
+          if ($releaseData !== null) {
+            $releases[] = $releaseData;
+          }
+        }
+      } else {
+        throw new \Exception('Unexpected response status: ' . $response->getStatusCode());
+      }
+    } catch (\Throwable $th) {
+      throw new \Exception('Unexpected response status: ' . $th->getMessage());
+    }
+
+    return $releases;
+  }
 }
