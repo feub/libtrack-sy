400738dbf2b9b130d5c4345d6f26eb7448b37ec4
Fabien Amann <fabien@feub.net>
2025-07-02 15:26:25 +0200

    client/feat: new Top 5 releases card

--- client/src/pages/StatsPage.tsx
+++ client/src/pages/StatsPage.tsx
@@ -15,7 +15,7 @@ export default function StatsPage() {
           <div className="flex flex-col gap-4 py-4 md:gap-4 md:py-4">
             <TopSectionCards />
 
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4 px-4 lg:px-4">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 px-4 lg:px-4">
               <FormatPieChartCard />
               <GenreBarChartCard />
             </div>
